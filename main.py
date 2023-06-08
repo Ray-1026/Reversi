2d52a2a9c3d39fa61823f15c1d1eee225750aee1
bill901230 <bill901230@gmail.com>
2023-06-08 18:47:33 +0800

    remove player_select

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     """
     pygame.init()
     main_clock = pygame.time.Clock()
-    first = "player"
+    _first = "player"
     game = GameLogic(first)
     board = Board(game)
     agent = Agent(first)
@@ -31,7 +31,7 @@ def main():
     button_white = text_white.get_rect(center=(390, 100))
     button_restart = text_restart.get_rect(center=(130, 300))
     button_quit = text_quit.get_rect(center=(310, 300))
-    player_select = False
+    #player_select = False
     at_start = False
     at_restart = False
     at_quit = False
@@ -73,7 +73,7 @@ def main():
                     """
                     按下start健
                     """
-                    if player_select and 315 <= x and x <= 390 and 25 <= y and y <= 75:
+                    if 315 <= x and x <= 390 and 25 <= y and y <= 75:
                         game = GameLogic(first)
                         board = Board(game)
                         agent = Agent(first)
@@ -85,10 +85,8 @@ def main():
                     選擇黑棋或白棋
                     """
                     if 265 <= x and x <= 345 and 80 <= y and y <= 130:
-                        player_select = True
                         first = "player"
                     elif 350 <= x and x <= 430 and 80 <= y and y <= 130:
-                        player_select = True
                         first = "computer"
 
             board.drawBoard(screen, game)
