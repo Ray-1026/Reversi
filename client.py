20a0faa48587723f23f5da078d817ab3d3a25826
Jay Lee <jayin920805@gmail.com>
2023-06-15 21:35:20 +0800

    remove redundant code in client.py

--- client.py
+++ client.py
@@ -15,32 +15,8 @@ def register_name(name, mode, s):
     time.sleep(0.5)    
     s.sendall(mode.encode())
     data = s.recv(1024)
-    if data == b'Connected':
-        # s.sendall(b'OK')
-        if mode == "active":
-            data = s.recv(8192)
-            print(pickle.loads(data))
-            while True:
-                oppo = input()
-                s.sendall(oppo.encode('utf-8'))
-                data = s.recv(8192).decode('utf-8')
-                print(data)
-                if data != "This user not available":
-                    break
-        elif mode == "passive":
-            while True:
-                data = s.recv(8192).decode('utf-8')
-                print(data)
-                if data.startswith("req"):
-                    confirm = input()
-                    s.sendall(confirm.encode('utf-8'))
-                    break
-            
-        while True:
-            color = s.recv(8192).decode('utf-8')
-            if color == "Black" or color == "White":
-                print("Start")
-                break
+
+    return data == b'Connected'
 
 def request_online_list(s):
     s.sendall('online_list'.encode())
@@ -49,63 +25,3 @@ def request_online_list(s):
     except:
         return -1
     return pickle.loads(online_list) 
-
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        name = input()
-        s.sendall(name.encode())
-        mode = input()
-        s.sendall(mode.encode())
-        data = s.recv(1024)
-        if data == b'Connected':
-            # s.sendall(b'OK')
-            if mode == "active":
-                data = s.recv(8192)
-                print(pickle.loads(data))
-                while True:
-                    oppo = input()
-                    s.sendall(oppo.encode('utf-8'))
-                    data = s.recv(8192).decode('utf-8')
-                    print(data)
-                    if data != "This user not available":
-                        break
-            elif mode == "passive":
-                while True:
-                    data = s.recv(8192).decode('utf-8')
-                    print(data)
-                    if data.startswith("req"):
-                        confirm = input()
-                        s.sendall(confirm.encode('utf-8'))
-                        break
-                
-            while True:
-                color = s.recv(8192).decode('utf-8')
-                print(color)
-                if color == "Black" or color == "White":
-                    print("Start")
-                    break
-    
-            while True:
-                if color == "Black":
-                    print("Black")
-                    move = input()
-                    print(f"Sending {move}")
-                    s.sendall(move.encode('utf-8'))
-                    data = s.recv(8192).decode('utf-8')
-                    print(data)
-                    if data == "END":
-                        break
-                elif color == "White":
-                    print("White")
-                    data = s.recv(8192).decode('utf-8')
-                    print(data)
-                    move = input()
-                    print(f"Sending {move}")
-                    s.sendall(move.encode('utf-8'))
-                    if data == "END":
-                        break
-                else:
-                    print("?")
-        else:
-            print(pickle.load(data))
